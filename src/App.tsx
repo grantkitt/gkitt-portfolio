45de7a2a64a63a860da85fee135a60b2754b9c00
grantkitt <38022110+grantkitt@users.noreply.github.com>
2023-12-16 18:52:50 -0600

    porty

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,12 @@ function App() {
 
 
   return (
-    <div className='flex flex-col w-screen items-center justify-start px-[1%] lg:px-[5%] pt-[2%] gap-y-20'>
-      <div className="flex flex-row items-start justify-center lg:justify-start w-full mb-[5%]">
-      <div className="flex flex-row items-end justify-between w-full lg:w-[45%] rounded-lg shadow-lg p-[2%]">
-      <h1 className='text-black font-bold text-[6vh] text-left leading-[50px]'>Grant Kitlowski</h1>
-      <a className="font-semibold text-lg text-gray-800" href="">RESUME</a>
-      <a className="font-semibold text-lg text-gray-800" href="">CONTACT</a>
-      </div>
+    <div className='flex flex-col w-full items-center justify-start lg:px-[5%] pt-[2%] gap-y-10 lg:gap-y-14 pb-[10%]'>
+      <div className="flex flex-row items-start justify-center w-full mb-[0.5%]">
+        <div className="flex flex-row items-end shadow-xl bg-blue-100 justify-between w-full lg:w-[55%] rounded-lg p-[4%]">
+          <h1 className='text-black font-bold text-2xl lg:text-[6vh] text-left leading-7 lg:leading-[50px]'>Grant Kitlowski</h1>
+          <a className="font-semibold text-xs lg:text-lg text-gray-800" href="mailto:gkitt@utexas.edu" target="_top">grant.kitlowski@gmail.com</a>
+        </div>
       </div>
     {components.components.map((component, index) => <Component {...component} key={index}/>)}
     </div>
--- src/components/component.tsx
+++ src/components/component.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 
 export interface componentProps {
     primarycolor: string;
@@ -6,6 +6,7 @@ export interface componentProps {
     title: string;
     role: string;
     description: string;
+    skills: string[];
     startdate: string;
     enddate: string;
     image: string;
@@ -15,8 +16,26 @@ export interface componentProps {
 export default function Component(props: componentProps) {
     const [isOpen, setIsOpen] = useState(false);
     const [isHover, setIsHover] = useState(false);
-    const primary = `bg-[${props.primarycolor}]`
-    const secondary = `text-[${props.secondarycolor}]`
+
+    const responsiveStyles = {
+        common: {
+          // Common styles
+          width: `${Math.floor(props.imagewidth / 2.5)}px`,
+          top: '-20px',
+          right: `-${Math.floor(props.imagewidth / 5)}px`,
+        },
+        mediaQuery: {
+            '@media (min-width: 1024px)': {
+              width: `${Math.floor(props.imagewidth * 0.9)}px`,
+              top: '-40px',
+              right: `-${Math.floor((props.imagewidth * 0.9) / 2)}px`,
+            },
+          },
+      };
+
+      const mediaQueryStyle = window.matchMedia('(min-width: 1024px)').matches
+      ? responsiveStyles.mediaQuery['@media (min-width: 1024px)']
+      : {};
 
 
     const handleClick = () => {
@@ -27,16 +46,41 @@ export default function Component(props: componentProps) {
         setIsHover(prev => !prev);
     }
 
+    const makeSkillString = () => {
+        let skillString = '';
+        props.skills.forEach(skill => {
+            if (skill === props.skills[props.skills.length - 1]) {
+                skillString += skill;
+                return;
+            }
+            skillString += skill + ', ';
+        })
+        return skillString;
+    }
+
     return (
         
-            <div onMouseEnter={handleHover} onMouseLeave={handleHover} className="shadow-lg hover:shadow-xl flex flex-col items-start justify-center px-[5%] py-[3%] cursor-pointer rounded-lg w-full lg:w-[80%] relative" style={{backgroundColor: props.primarycolor}}>
-                <h1 className="text-3xl font-bold" style={{color: props.secondarycolor}}>{props.title}</h1>
+            <div className='w-[90%] lg:w-[80%] flex flex-col items-center justify-center relative'>
+                    <div onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHover} className="shadow-lg hover:shadow-xl flex flex-col items-start justify-center px-[5%] py-[5%] cursor-pointer rounded-lg relative w-full" style={{backgroundColor: props.primarycolor}}>
+                    <h1 className="text-lg font-bold" style={{color: props.secondarycolor}}>{props.title}</h1>
 
-                <h2 className="text-lg font-semibold italic mr-5 leading-5" style={{color: props.secondarycolor}}>{props.role}</h2>
-                <h2 className="text-xs font-semibold" style={{color: props.secondarycolor}}>{props.startdate} - {props.enddate}</h2>
-        
-            <div className='absolute top-0 right-0 rounded-bl-full h-[80px] w-[80px]' style={{backgroundColor: props.secondarycolor}}></div> 
-            <img width={`${props.imagewidth}px`} style={{top: `-50px`, right: `-${parseInt(props.imagewidth / 2)}px`}} className={`absolute ${isHover ? 'rotate-[30deg]' : 'rotate-[10deg]'} transform transition duration-100`} src={props.image} alt="" />
+                    <h2 className="text-sm font-semibold italic mr-5 leading-5" style={{color: props.secondarycolor}}>{props.role}</h2>
+                    <h2 className="text-xs font-semibold" style={{color: props.secondarycolor}}>{props.startdate} - {props.enddate}</h2>
+            
+                    <div className='absolute top-0 right-0 rounded-tr-lg rounded-bl-full h-[30px] w-[30px] lg:h-[60px] lg:w-[60px]' style={{backgroundColor: props.secondarycolor}}></div> 
+                    <div style={{
+                        ...responsiveStyles.common,
+                        ...mediaQueryStyle,}}
+                    className={`absolute ${isHover || isOpen ? 'rotate-[30deg]' : 'rotate-[10deg]'} transform transition duration-100`}>
+                    <img src={props.image} alt="" />
+                    </div>
+                </div>
+                
+                <div className={`flex flex-col items-start justify-start transition-all duration-300 ease-in-out overflow-hidden w-full px-[2%] pb-[1%] max-h-0 ${isOpen ? 'max-h-[1000px]' : 'max-h-0 '}`}>
+                    <p className="text-sm text-left lg:text-lg mt-5 mb-3 lg:mb-5 leading-4 lg:leading-7" style={{color: props.primarycolor}}>{props.description}</p>
+                    <h2 className='italic font-semibold text-[10px] lg:text-sm' style={{color: props.primarycolor}}>{makeSkillString()}</h2>
+                </div>
+                
             </div>
     )
 
